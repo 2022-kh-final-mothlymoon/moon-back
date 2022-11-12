68968dbfcb49d1c5d9c594cb277d34d59804976d
rlagmlfk <gmlfk703@gmail.com>
2022-11-12 20:13:26 +0900

    221112 스프링 최종

--- sellermoon/src/main/java/kh/sellermoon/member/logic/PCartLogic.java
+++ sellermoon/src/main/java/kh/sellermoon/member/logic/PCartLogic.java
@@ -1,6 +1,10 @@
 package kh.sellermoon.member.logic;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -31,16 +35,16 @@ public class PCartLogic {
 		// 장바구니에 담긴 특정 상품 개수, cartNo 조회
 		Map<String, Object> existMap = pCartDao.existMdInCart(pMap);
 		logger.info("existMap0:" + existMap);
-		// 이미 장바구니에 해당 상품이 존재한다면
+		// 장바구니에 이미 해당 상품이 존재하니?
 		if(existMap != null) {
-			// cnt = 이미 장바구니에 담은 해당 상품 갯수
+			// 이미 장바구니에 담은 해당 상품 갯수
 			int cnt =  Integer.parseInt(existMap.get("CARTQUANTITY").toString()); 
 			logger.info("cnt:" + cnt);
+			
 			// 새로 담으려는 상품 갯수
 			int quantity = Integer.parseInt(pMap.get("cartQuantity").toString()); 
 			logger.info("quantity:" + quantity);
 			
-			// quantity = quantity + cnt
 			quantity += cnt; 
 
 			pMap.put("quantity", quantity);
@@ -63,5 +67,32 @@ public class PCartLogic {
 
 	public void deleteCart(Map<String, Object> cartMap) throws Exception {
 		pCartDao.deleteCart(cartMap);
+	}
+
+	public void orderCart(Map<String, Object> pMap) throws Exception {
+		String orderType= pMap.get("orderType").toString();
+		
+		if(orderType.equals("T")) {
+			//String orderNumber = pCartDao.getOrderNumber();
+			//테이블 연관관계 수정되면 위의 주석 해제, 아래 구문 삭제하기! -- 지금은 상수 값 "2"으로 박아놓음...
+			String orderNumber = "2"; 
+			String date = pMap.get("date").toString();
+			
+			date = date.replace('T', ' ');
+			date = date.replace('Z', ' ');
+			
+			pMap.put("orderNo", orderNumber);
+			pMap.put("startDate", date);
+		
+			pCartDao.insertSubs(pMap);
+		}
+		
+		List<String> arr = (List<String>) pMap.get("cartNo");
+		
+		for(String no : arr) {
+			logger.info(no);
+			pMap.put("cartNo", no);
+			pCartDao.deleteCart(pMap);
+		}
 	}
 }
--- sellermoon/src/main/java/kh/sellermoon/member/vo/CartVO.java
+++ sellermoon/src/main/java/kh/sellermoon/member/vo/CartVO.java
@@ -2,7 +2,6 @@ package kh.sellermoon.member.vo;
 
 import lombok.Data;
 
-@Data
 public class CartVO {
 
 	private String cartNo;
@@ -10,7 +9,54 @@ public class CartVO {
 	private int    mdNo;    
 	private int    cartQuantity;       
 	private String orderType;
+	private String orderYn;
 	private MdVO mdVO;
 	
-	
+	public String getCartNo() {
+		return cartNo;
+	}
+	public void setCartNo(String cartNo) {
+		this.cartNo = cartNo;
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public int getMdNo() {
+		return mdNo;
+	}
+	public void setMdNo(int mdNo) {
+		this.mdNo = mdNo;
+	}
+	public int getCartQuantity() {
+		return cartQuantity;
+	}
+	public void setCartQuantity(int cartQuantity) {
+		this.cartQuantity = cartQuantity;
+	}
+	public String getOrderType() {
+		return orderType;
+	}
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+	}
+	public MdVO getMdVO() {
+		return mdVO;
+	}
+	public void setMdVO(MdVO mdVO) {
+		this.mdVO = mdVO;
+	}
+	public String getOrderYn() {
+		return orderYn;
+	}
+	public void setOrderYn(String orderYn) {
+		this.orderYn = orderYn;
+	}
+	@Override
+	public String toString() {
+		return "CartVO [cartNo=" + cartNo + ", memberNo=" + memberNo + ", mdNo=" + mdNo + ", cartQuantity="
+				+ cartQuantity + ", orderType=" + orderType + ", orderYn=" + orderYn + ", mdVO=" + mdVO + "]";
+	}
 }
